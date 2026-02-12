94f8cb4f827ee0c154236d07433c7533571ddef4
Ponnuri Pavan Kumar <pavankumar.prrp@gmail.com>
2026-02-12 13:09:14 +0530

    Changed the port number in server.js and updated the UI.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
